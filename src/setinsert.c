71c47928afbdafa41cc9b645a8807f28c0277e92
momtx <momtx@localhost>
2015-01-01 17:05:14 +0100

    Documentation

--- src/setinsert.c
+++ src/setinsert.c
@@ -1,18 +1,15 @@
-/* ============================= C MeatAxe ==================================
-   File:        $Id: setinsert.c,v 1.1.1.1 2007/09/02 11:06:17 mringe Exp $
-   Comment:     Insert an element into a set.
-   --------------------------------------------------------------------------
-   (C) Copyright 1998 Michael Ringe, Lehrstuhl D fuer Mathematik,
-   RWTH Aachen, Germany  <mringe@math.rwth-aachen.de>
-   This program is free software; see the file COPYING for details.
-   ========================================================================== */
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// C MeatAxe - Insert an element into a set
+//
+// (C) Copyright 1998-2015 Michael Ringe, Lehrstuhl D fuer Mathematik, RWTH Aachen
+//
+// This program is free software; see the file COPYING for details.
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "meataxe.h"
 
-   
-/* --------------------------------------------------------------------------
-   Local data
-   -------------------------------------------------------------------------- */
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Local data
 
 MTX_DEFINE_FILE_INFO
 
@@ -21,6 +18,7 @@ const int BlockSize = 5;    /* Allocation unit */
 /// @addtogroup intset
 /// @{
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Insert an element into a set.
 /// @param set Pointer to the set.
 /// @param elem Number to insert.
@@ -28,48 +26,45 @@ const int BlockSize = 5;    /* Allocation unit */
 
 int SetInsert(Set_t *set, long elem)
 {
-    register int i, k;
-    register long *d;
+   register int i, k;
+   register long *d;
 
-    if (!SetIsValid(set))
-    {
-	MTX_ERROR1("set: %E",MTX_ERR_BADARG);
-	return -1;
-    }
+   if (!SetIsValid(set)) {
+      MTX_ERROR1("set: %E",MTX_ERR_BADARG);
+      return -1;
+   }
 
-    /* Find the position of <elem> in the data buffer.
-       ----------------------------------------------- */
-    d = set->Data;
-    for (i = 0; i < set->Size && d[i] < elem; ++i);
+   // Find the position of <elem> in the data buffer.
+   // If <elem> is already containd in the set, exit.
+   // TODO: binary search
+   d = set->Data;
+   for (i = 0; i < set->Size && d[i] < elem; ++i) {
+   }
+   if ((i < set->Size) && (d[i] == elem)) {
+      return 0;
+   }
 
-    /* If <elem> is already containd in the set, exit.
-       ----------------------------------------------- */
-    if (i < set->Size && d[i] == elem) 
-	return 0;
+   // extend the buffer if necessary
+   if (set->Size >= set->BufSize) {
+      int newmax = set->BufSize + BlockSize;
+      long *newbuf = NREALLOC(set->Data,long,newmax);
+      if (newbuf == NULL) {
+         MTX_ERROR("Cannot grow set");
+         return -1;
+      }
+      set->BufSize = newmax;
+      d = set->Data = newbuf;
+   }
 
-    /* Extend the set, if necessary
-       ---------------------------- */
-    if (set->Size >= set->BufSize)
-    {
-	int newmax = set->BufSize + BlockSize;
-	long *newbuf = NREALLOC(set->Data,long,newmax);
-	if (newbuf == NULL)
-	{
-	    MTX_ERROR("Cannot grow set");
-	    return -1;
-	}
-	set->BufSize = newmax;
-	d = set->Data = newbuf;
-    }
+   // insert new element
+   for (k = set->Size - 1; k >= i; --k) {
+      d[k + 1] = d[k];
+   }
+   d[i] = elem;
+   ++set->Size;
 
-    /* Insert the new element into the data buffer
-       ------------------------------------------- */
-    for (k = set->Size - 1; k >= i; --k)
-	d[k+1] = d[k];
-    d[i] = elem;
-    ++set->Size;
-
-    return 0;
+   return 0;
 }
+
 
 /// @}
--- src/settest.c
+++ src/settest.c
@@ -1,42 +1,41 @@
-/* ============================= C MeatAxe ==================================
-   File:        $Id: settest.c,v 1.1.1.1 2007/09/02 11:06:17 mringe Exp $
-   Comment:     Check if a set contains an element.
-   --------------------------------------------------------------------------
-   (C) Copyright 1998 Michael Ringe, Lehrstuhl D fuer Mathematik,
-   RWTH Aachen, Germany  <mringe@math.rwth-aachen.de>
-   This program is free software; see the file COPYING for details.
-   ========================================================================== */
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// C MeatAxe - Check if a set contains an element
+//
+// (C) Copyright 1998-2015 Michael Ringe, Lehrstuhl D fuer Mathematik, RWTH Aachen
+//
+// This program is free software; see the file COPYING for details.
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "meataxe.h"
 
-   
-/* --------------------------------------------------------------------------
-   Local data
-   -------------------------------------------------------------------------- */
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Local data
 
 MTX_DEFINE_FILE_INFO
 
 /// @addtogroup intset
 /// @{
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Check if a number is in a set.
 /// @param set Pointer to the set.
 /// @param elem Number to check.
 /// @return 1 if the set contains @a elem, 0 if not.
 
 int SetContains(const Set_t *set, long elem)
-
 {
-    int i;
-    long *l;
- 
-    if (!SetIsValid(set))
-    {
-	MTX_ERROR1("%E",MTX_ERR_BADARG);
-	return -1;
-    }
-    l = set->Data;
-    for (i = set->Size; i > 0 && *l < elem; --i, ++l);
-    return (i > 0 && *l == elem);
+   int i;
+   long *l;
+
+   if (!SetIsValid(set)) {
+      MTX_ERROR1("%E",MTX_ERR_BADARG);
+      return -1;
+   }
+   l = set->Data;
+   for (i = set->Size; i > 0 && *l < elem; --i, ++l) {
+   }
+   return i > 0 && *l == elem;
 }
+
+
 /// @}
